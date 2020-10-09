a70cd6afc01858e03946a4ef92dfa002115308da
RodrigoSebastian <57414811+RodrigoSebastian@users.noreply.github.com>
2020-10-08 19:21:23 -0500

    Agregar font y perfil_layout

--- LumaApp/app/src/main/java/mx/tec/lumaapp/MainActivity.kt
+++ LumaApp/app/src/main/java/mx/tec/lumaapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package mx.tec.lumaapp
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.perfil_layout)
+
+        val miPerfil = findViewById<TextView>(R.id.miPerfil)
+
+        miPerfil.typeface = Typeface.createFromAsset(assets,"Fonts/Righteous-Regular.ttf")
+
     }
 }
