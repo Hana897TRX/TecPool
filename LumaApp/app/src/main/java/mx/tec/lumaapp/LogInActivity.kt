68dbe37aa40c4d046188c40e79ed1b949877c9bd
Hana897TRX <57414872+Hana897TRX@users.noreply.github.com>
2020-10-21 16:59:38 -0500

    First DB and LogIn Version v1.0.0 You can register default users with "d" letter in password input and any User

--- LumaApp/app/src/main/java/mx/tec/lumaapp/LogInActivity.kt
+++ LumaApp/app/src/main/java/mx/tec/lumaapp/LogInActivity.kt
@@ -3,15 +3,22 @@ package mx.tec.lumaapp
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.login_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import mx.tec.lumaapp.db.config.DBAccess
 
-class LogInActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity(), DBAccess.OnTaskCompleted {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_main)
@@ -29,70 +36,19 @@ class LogInActivity : AppCompatActivity() {
         val mantener = findViewById<CheckBox>(R.id.manter_iniciadoBtn)
 
         btnAceptar.setOnClickListener {
-            validadUsuario_JarCodeado(userTxt.text.toString(),passwordTxt.text.toString(),mantener)
+            var dbAccess : DBAccess = DBAccess(this)
+            dbAccess.execute(userTxt.text.toString(), passwordTxt.text.toString(), "0")
         }
     }
 
-    fun validadUsuario_JarCodeado(user: String, password: String, mantener: CheckBox) {
-
-        val UsusariosJar = arrayListOf<Usuario_JarCodeado>(
-            Usuario_JarCodeado(
-                "Gabriela Fernanda Soto GUTIERREZ",
-                "Gaby",
-                "Gaby",
-                "7771033890",
-                20,
-                "ITC",
-                30
-            ),
-            Usuario_JarCodeado("Raúl Orihuela Rosas", "Rulo", "Rulo", "7771046716", 22, "ITC", 300),
-            Usuario_JarCodeado(
-                "Israel Sánchez Hinojosa",
-                "Montoya",
-                "Montoya",
-                "7773401499",
-                20,
-                "ITC",
-                0
-            ),
-            Usuario_JarCodeado(
-                "Rodrigo Sebastián de la Rosa Andrés",
-                "Tory",
-                "Tory",
-                "7472211787",
-                20,
-                "ITC",
-                100
-            )
-        )
-
-        UsusariosJar.forEach {
-
-            if (user == it.user) {
-                if (password == it.password) {
-                    val sharedPreferences =
-                        getSharedPreferences("informacion_usuario", Context.MODE_PRIVATE)
-
-                    with(sharedPreferences.edit()) {
-                        putString("nombre", it.nombre)
-                        if (mantener.isChecked) {
-                            putInt("mantener",1)
-                            putString("user", it.user)
-                            putString("password", it.password)
-                        } else
-                            putInt("mantener",0)
-                        putString("telefono", it.telefono)
-                        putInt("edad", it.edad)
-                        putString("carrera", it.carrera)
-                        putInt("ecosAcumulados", it.ecosAcumulados)
-                        commit()
-                    }
-
-                    val intent = Intent(this, MainActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                    startActivity(intent)
-                }
-            }
+    override fun onTaskCompleted(response: Boolean) {
+        if(response){
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+        else{
+            Toast.makeText(this, "Usuario no valido", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -101,14 +57,4 @@ class LogInActivity : AppCompatActivity() {
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
     }
-
-    data class Usuario_JarCodeado(
-        val nombre: String,
-        val user: String,
-        val password: String,
-        val telefono: String,
-        val edad: Int,
-        val carrera: String,
-        val ecosAcumulados: Int
-    ){}
 }
---
+++ LumaApp/app/src/main/java/mx/tec/lumaapp/db/dao/UsuariosCore.kt
@@ -0,0 +1,20 @@
+package mx.tec.lumaapp.db.dao
+
+import androidx.room.*
+import mx.tec.lumaapp.db.model.Usuario
+
+@Dao
+interface UsuariosCore {
+    @Query("SELECT * FROM Usuario")
+    fun obtenerUsuarios() : List<Usuario>
+    @Query("SELECT * FROM Usuario WHERE Username LIKE :username")
+    fun obtenerUsuario(username : String) : Usuario
+    @Insert
+    fun registrarUsuarios(user: Usuario)
+    @Update
+    fun updateUser(user : Usuario)
+    @Query("UPDATE Usuario SET EcoPuntos = :ecoPuntos WHERE Id LIKE :id")
+    fun updateEcoPuntos(id : Int, ecoPuntos : Int)
+    @Delete
+    fun deleteUser(user : Usuario)
+}
