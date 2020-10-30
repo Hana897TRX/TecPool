b68ae966ae3f0e9d8c1302c79f176cccf5b81b19
RodrigoSebastian <57414811+RodrigoSebastian@users.noreply.github.com>
2020-10-30 00:23:55 -0600

    - Logo para la app

--- LumaApp/app/src/main/java/mx/tec/lumaapp/MainActivity.kt
+++ LumaApp/app/src/main/java/mx/tec/lumaapp/MainActivity.kt
@@ -1,25 +1,19 @@
 package mx.tec.lumaapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.GestureDetector
-import android.view.Menu
-import android.view.MenuItem
 import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GestureDetectorCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,14 +26,9 @@ class MainActivity : AppCompatActivity() {
         draw.open()
         toggle.syncState()
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         val navController = navHostFragment.navController
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
-
-
-//        val navView = findViewById<NavigationView>(R.id.fragment)
-//        navView.setNavigationItemSelectedListener(this)
     }
 }
