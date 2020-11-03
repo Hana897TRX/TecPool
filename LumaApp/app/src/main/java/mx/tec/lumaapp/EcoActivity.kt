787bc5bb12933378cbe10c4a9921fb297bfd5b09
Israel Sanchez <ishisra14@hotmail.com>
2020-11-03 01:51:54 -0600

    Aqui jalo mi primer aporte al proyecto despues de 10 semanas xD Pd. Agrega el recycler view de los ecopuntos corregido (por ahora)

--- LumaApp/app/src/main/java/mx/tec/lumaapp/EcoActivity.kt
+++ LumaApp/app/src/main/java/mx/tec/lumaapp/EcoActivity.kt
@@ -1,16 +1,15 @@
 package mx.tec.lumaapp
 
+import android.Manifest
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.eco_puntos_layout.*
 import mx.tec.lumaapp.elementos_recycler.adapter.CuponesAdapter
 import mx.tec.lumaapp.elementos_recycler.model.CuponesModel
 
@@ -20,18 +19,20 @@ class EcoActivity : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val EcoPts = view?.findViewById<TextView>(R.id.txtPuntosTotales)
+
         val view = inflater.inflate(R.layout.eco_puntos_layout, container, false)
 
         val cupones = arrayListOf<CuponesModel>(
             CuponesModel("Comida", 100, R.drawable.regalo),
-            CuponesModel("Entretenimiento", 150, R.drawable.regalo),
+            CuponesModel("Diversion", 150, R.drawable.regalo),
             CuponesModel("???", 200, R.drawable.regalo),
-            )
+        )
 
         val adaptador = CuponesAdapter(this.requireContext(), R.layout.cupones_layout, cupones)
 
         val rvCupones = view.findViewById<RecyclerView>(R.id.rvCupones)
-        rvCupones.layoutManager = StaggeredGridLayoutManager(3, GridLayoutManager.HORIZONTAL)
+        rvCupones.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
         rvCupones.setHasFixedSize(true)
         rvCupones.adapter = adaptador
 
