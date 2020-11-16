b0843171be1911093eb0a43346b2df72050026ba
Hana897TRX <57414872+Hana897TRX@users.noreply.github.com>
2020-11-16 16:03:47 -0600

    New Bottom Menu

--- LumaApp/app/src/main/java/mx/tec/lumaapp/MainActivity.kt
+++ LumaApp/app/src/main/java/mx/tec/lumaapp/MainActivity.kt
@@ -9,18 +9,27 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GestureDetectorCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navHostFragment : NavHostFragment
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        findViewById<NavigationView>(R.id.nav_view)
-            .setupWithNavController(navController)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        var nav = findViewById<BottomNavigationView>(R.id.buttomNavigationView)
+        nav.setupWithNavController(navController)
+        navController.graph = navController.navInflater.inflate(R.navigation.main_navigation)
     }
 }
