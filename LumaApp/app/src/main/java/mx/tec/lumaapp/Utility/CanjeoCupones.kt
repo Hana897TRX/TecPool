ca57991fb1eaff439d4d12b8489fb58d66ff87c4
RodrigoSebastian <57414811+RodrigoSebastian@users.noreply.github.com>
2020-11-30 13:17:52 -0600

    - Nada 2

--- LumaApp/app/src/main/java/mx/tec/lumaapp/Utility/CanjeoCupones.kt
+++ LumaApp/app/src/main/java/mx/tec/lumaapp/Utility/CanjeoCupones.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import mx.tec.lumaapp.R
 
 class CanjeoCupones : Fragment() {
+
+    private var cant = 1
+    var precio = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +20,30 @@ class CanjeoCupones : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.canjeo_cupones, container, false)
+
+        val agregarBtn = view.findViewById<ImageButton>(R.id.masBtn)
+        val menosBtn = view.findViewById<ImageButton>(R.id.menosBtn)
+        val cantidad  = view.findViewById<TextView>(R.id.cantidadTxt)
+        val costo = view.findViewById<TextView>(R.id.costoTxt)
+
+        cantidad.text = cant.toString()
+
+        agregarBtn.setOnClickListener{
+            cant++
+            cantidad.text = cant.toString()
+            precio = cant * 20
+
+            costo.text = "Total: $precio Ecopuntos"
+        }
+
+        menosBtn.setOnClickListener {
+            if (cant > 1) {
+                cant--
+                cantidad.text = cant.toString()
+                precio = cant * 20
+                costo.text = "Total: $precio Ecopuntos"
+            }
+        }
 
         return view
     }
